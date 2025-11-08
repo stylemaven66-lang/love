6cd1e54be575fdb6f8bba4dc19d02133651589de
stylemaven66-lang <stylemaven66@gmail.com>
2025-11-08 10:51:34 +0500

    Create playlist.js

---
+++ assets/js/playlist.js
@@ -0,0 +1,19 @@
+export const playlist = [
+    {
+        title: 'Tender Nights',
+        artist: 'Molu',
+        // Royalty-free demo track URL (public sample)
+        src: 'https://cdn.pixabay.com/download/audio/2022/03/15/audio_2e7ef2d6f8.mp3?filename=romantic-epic-111194.mp3'
+    },
+    {
+        title: 'Moonlit Walk',
+        artist: 'Molu',
+        src: 'https://cdn.pixabay.com/download/audio/2022/10/21/audio_3eaa1bcdba.mp3?filename=soft-piano-ambient-124475.mp3'
+    },
+    {
+        title: 'Heartbeat Waltz',
+        artist: 'Molu',
+        src: 'https://cdn.pixabay.com/download/audio/2022/03/15/audio_6e7d11e3f0.mp3?filename=romantic-118157.mp3'
+    }
+];
+
